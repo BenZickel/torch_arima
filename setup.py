7e4257ecd9e9c5c797216031d61562fd7984ed54
Ben Zickel <benzickel@gmail.com>
2024-11-03 19:20:07 +0200

    Add git commit hash to version and pyproject.toml.

--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
-import setuptools
+
+import setuptools, subprocess
+
+# Baseline version identifier
+version='0.0.6'
+
+# Try to add commit hash to version
+try:
+    commit_sha = subprocess.check_output(
+        ["git", "rev-parse", "--short", "HEAD"]).decode("ascii").strip()
+    version += f"+{commit_sha}"
+# catch all exception to be safe
+except Exception:
+    pass  # probably not a git repo
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name='torch_arima',
-    version='0.0.5',
+    version=version,
     long_description=long_description,
     long_description_content_type="text/markdown",
     description='ARIMA time series implementation in PyTorch and Pyro',
